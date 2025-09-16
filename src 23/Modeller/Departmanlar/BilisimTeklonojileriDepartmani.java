6deead14d304e190e0514665dff3b4a744d010fb
Gul Seyma Onol <g.cibisoglu92@gmail.com>
2025-09-16 11:48:09 +0300

    employee created

---
+++ src 23/Modeller/Departmanlar/BilisimTeklonojileriDepartmani.java
@@ -0,0 +1,23 @@
+package Modeller.Departmanlar;
+
+public class BilisimTeklonojileriDepartmani implements Departman {
+
+    final String departmanKodu = "BTD";
+
+    // TODO Bu class'da sadece zam oranini belirtmelisiniz
+    //      Başka bişeyi değiştirmeye gerek yok.
+    final int zamOrani = 0;
+
+
+    @Override
+    public int getZamOrani() {
+
+        return zamOrani;
+    }
+
+    @Override
+    public String getDepartmanKodu() {
+
+        return departmanKodu;
+    }
+}
